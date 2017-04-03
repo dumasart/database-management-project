bf7af7b14cc57bad02a38b11461025d5f3e3afe0
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2017-04-03 16:09:21 +0200

    ajout package back end

---
+++ src/BackEnd/Getter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackEnd;
+
+/**
+ *
+ * @author milang
+ * class local qui ne sert qu'à effectuer les requètes 
+ */
+class Getter {
+    
+}
