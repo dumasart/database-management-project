33e58809eae325f88d0a4a821928ab2ea4d34bd4
Gaël Nomézine <gael.nomezine@ensimag.grenoble-inp.fr>
2017-04-09 22:14:05 +0200

    Ajouté le squelette pour le DAO artiste
    les méthodes ne sont pas implémentées

---
+++ src/Model/DataAccessLayer/ArtisteDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.DataAccessLayer;
+
+import Model.Business.Artiste;
+
+/**
+ *
+ * @author nomezing
+ */
+public interface ArtisteDAO {
+    public boolean update(Artiste artiste);
+    public boolean insert(Artiste artiste);
+    public boolean delete(Artiste artiste);
+}
---
+++ src/Model/DataAccessLayer/ArtisteDAOSQL.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.DataAccessLayer;
+
+import Model.Business.Artiste;
+
+/**
+ *
+ * @author nomezing
+ */
+public class ArtisteDAOSQL implements ArtisteDAO {
+
+    @Override
+    public boolean update(Artiste artiste) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean insert(Artiste artiste) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Artiste artiste) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
