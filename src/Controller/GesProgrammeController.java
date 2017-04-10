831743ddd3e219498e10135eef07d5ed947c3df7
Mamady Nabé <mamady.nabe@phelma.grenoble-inp.fr>
2017-04-10 16:45:00 +0200

    Rapport

--- src/Controller/GesProgrammeController.java
+++ src/Controller/GesProgrammeController.java
@@ -8,12 +8,8 @@ package Controller;
 import Model.DataTransfertObject.NumeroDAO;
 import Model.DataTransfertObject.NumeroDAOSQL;
 import Model.DataTransfertObject.SpectacleDAO;
-<<<<<<< HEAD
-import Model.DataTransfertObject.SpectacleDOASQL;
 import Model.Numero;
-=======
 import Model.DataTransfertObject.SpectacleDAOSQL;
->>>>>>> 4df04405f9a82d5a0c772914e02eec5d39f98f2a
 import Model.Spectacle;
 import java.util.List;
 
@@ -23,7 +19,7 @@ import java.util.List;
  */
 public class GesProgrammeController extends MainController {
 
-    private SpectacleDAO spectacleDAO = new SpectacleDOASQL();
+    private SpectacleDAO spectacleDAO = new SpectacleDAOSQL();
     private NumeroDAO numeroDAO = new NumeroDAOSQL();
     
     
