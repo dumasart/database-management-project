211186490e4c51bc5f9e29fe0ae73a58b1b87944
Duc-Anh Luu <Duc-Anh.Luu@grenoble-inp.org>
2017-04-10 16:19:38 +0200

    ajoute l'association un numero avec un expert

--- src/Model/DataTransfertObject/EvaluationDAOSQL.java
+++ src/Model/DataTransfertObject/EvaluationDAOSQL.java
@@ -19,62 +19,78 @@ import java.util.List;
  * @author milang
  */
 public class EvaluationDAOSQL implements EvaluationDAO {
-    
+
     //TODO à voir si on en à besoin car on est parti du principe qu'une
     // évaluation ne peut pas etre modifiée après validation (saisie)
     @Override
     public boolean update(Evaluation evaluation) {
-        String cmd = "UPDATE Evaluation SET note = " +
-                evaluation.getNote() +
-                " AND evaluation = " +
-                evaluation.getEvaluation() +
-                " WHERE codeArtiste = " +
-                evaluation.getID() +
-                " AND codeNumero = " + 
-                evaluation.getCodeNumero();
-        int res=Getter.update(cmd);
-        if(res==0)
+        String cmd = "UPDATE Evaluation SET note = "
+                + evaluation.getNote()
+                + " AND evaluation = "
+                + evaluation.getEvaluation()
+                + " WHERE codeArtiste = "
+                + evaluation.getID()
+                + " AND codeNumero = "
+                + evaluation.getCodeNumero();
+        int res = Getter.update(cmd);
+        if (res == 0) {
             return false;
-        else 
+        } else {
             return true;
+        }
     }
 
     @Override
     public boolean delete(Evaluation evaluation) {
-        String cmd = "DELETE from Evaluation WHERE codeArtiste = "+
-                evaluation.getID()+
-                " AND codeArtiste = "+
-                evaluation.getCodeNumero();
-        int res=Getter.update(cmd);
-        if(res==0)
+        String cmd = "DELETE from Evaluation WHERE codeArtiste = "
+                + evaluation.getID()
+                + " AND codeArtiste = "
+                + evaluation.getCodeNumero();
+        int res = Getter.update(cmd);
+        if (res == 0) {
             return false;
-        else 
+        } else {
             return true;
+        }
     }
 
     @Override
-    public boolean insert(Evaluation evaluation,Numero numero,Expert expert) {
+    public boolean insert(Evaluation evaluation, Numero numero, Expert expert) {
         String cmd = "INSERT INTO Evaluation (codeArtiste, codeNumero, evaluation, note) VALUES (" + expert.getId() + " , "
-                    + numero.getID() + " , " + evaluation.getEvaluation() + " , " + evaluation.getNote() + " ) ";
-        int res=Getter.update(cmd);
-        if(res==0)
+                + numero.getID() + " , " + evaluation.getEvaluation() + " , " + evaluation.getNote() + " ) ";
+        int res = Getter.update(cmd);
+        if (res == 0) {
             return false;
-        else 
+        } else {
             return true;
+        }
+    }
+
+    /**
+     * Affecte un numéro à un expert
+     *
+     * @param exp
+     * @param numero
+     */
+    public boolean insert(Expert exp, Numero numero) {
+        String cmd = "INSERT INTO Evaluation VALUES (" + exp.getId() + ", " + numero.getID() + ")";
+        Getter.update(cmd);
+        exp.ajouteNumero(numero);
+        return true;
     }
 
     @Override
     public List<Evaluation> getEvaluationFromNumero(Numero numero) {
         String cmd = "SELECT * FROM Evaluation WHERE codeNumero = " + numero.getID();
-        List<Evaluation> res = new ArrayList<>(); 
+        List<Evaluation> res = new ArrayList<>();
         try {
             ResultSet b = Getter.request(cmd);
             while (b.next()) {
                 Evaluation eval = new Evaluation(
-                    b.getInt("codeArtiste"),
-                    b.getInt("codeNumero"),
-                    b.getString("evaluation"),
-                    b.getInt("note")
+                        b.getInt("codeArtiste"),
+                        b.getInt("codeNumero"),
+                        b.getString("evaluation"),
+                        b.getInt("note")
                 );
                 res.add(eval);
             }
@@ -82,5 +98,5 @@ public class EvaluationDAOSQL implements EvaluationDAO {
             System.out.println("Erreur SQL : Numéro inconnu");
         }
         return res;
-    }    
+    }
 }
